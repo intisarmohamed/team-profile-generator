777b278538a2c3a077c65bfce68f8e2542032214
Intisar Mohamed <Intisarm48@gmail.com>
2021-12-05 08:40:23 +0000

    created files,classes and questions

---
+++ src/lib/Engineer.js
@@ -0,0 +1,15 @@
+class Engineer extends Employee {
+  constructor({ name, id, email, github }) {
+    super({ id, name, email });
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+
+  getRole() {
+    return "Engineer";
+  }
+}
+module.exports = Engineer;
---
+++ src/lib/Manager.js
@@ -0,0 +1,15 @@
+class Manager extends Employee {
+  constructor({ name, id, email, officeNumber }) {
+    super({ name, id, email, officeNumber });
+    this.officeNumber = officeNumber;
+  }
+
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+
+  getRole() {
+    return "Manager";
+  }
+}
+module.exports = Manager;
